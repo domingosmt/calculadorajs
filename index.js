84b5c906f84ed75b4316bfb009a2a0464e47e601
DomingosMt <domingosmuratori@outlook.com>
2026-05-14 21:59:26 -0300

    feature: js add and dom manipl

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 function calcular() {
-    const numero1 = document.getElementById("numero1").value
-    const numero2 = document.getElementById("numero2").value
+    const numero1 = Number(document.getElementById("numero1").value)
+    const numero2 = Number(document.getElementById("numero2").value)
     const operador = document.getElementById("operador").value
+    const resultado = document.getElementById("resultado")
+
     console.log(numero1)
     console.log(numero2)
     console.log(operador)
+
+    let resposta;
+
+    if (operador == "+") {
+        resposta = numero1 + numero2
+    } else if (operador == "-") {
+        resposta = numero1 - numero2
+    } else if (operador == "*") {
+        resposta = numero1 * numero2
+    } else if (operador == "/") {
+        resposta = numero1 / numero2
+    } else {
+        ErrorEvent
+    }
+
+    console.log(resposta)
+
+    resultado.innerHTML = `O resultado de ${numero1} ${operador} ${numero2} = ${resposta}`
 }
