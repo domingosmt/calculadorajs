550f7bc662ae51476e37bf8bfb5e31ceef04cfc0
DomingosMt <domingosmuratori@outlook.com>
2026-05-14 21:47:45 -0300

    feature: html, css and js structure

---
+++ index.js
@@ -0,0 +1,8 @@
+function calcular() {
+    const numero1 = document.getElementById("numero1").value
+    const numero2 = document.getElementById("numero2").value
+    const operador = document.getElementById("operador").value
+    console.log(numero1)
+    console.log(numero2)
+    console.log(operador)
+}
